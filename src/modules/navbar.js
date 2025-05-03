616ed267e2f279e86258a3ea68f0f2563300ad09
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-05-03 16:25:24 -0300

    style: adiciona estilos do modo escuro

--- src/modules/navbar.js
+++ src/modules/navbar.js
@@ -59,31 +59,3 @@ window.addEventListener('load', updateActiveNavLink);
 // Executar inicialmente para definir estado correto
 updateActiveNavLink();
 // Active Nav Link End
-
-const $navbarCollapse = document.querySelector("#collapse");
-const $navbarCollapseUl = document.querySelector(".offcanvas-body ul");
-const btnlg = `<button type="button" class="btn btn-primary btn-lg" data-bs-toggle="modal" data-bs-target="#loginSignUp">Acessar</button>`;
-let responseWindow = () => {
-  let widht = window.innerWidth;
-  const btnQuantity = document.querySelectorAll(".btn-lg");
-  const arrayBtns = Array.from(btnQuantity);
-  arrayBtns.forEach((btn, index) => {
-    if (widht > 992) {
-      if (index == 0) {
-        btn.remove();
-      }
-    } else {
-      if (index == 0) {
-        btn.remove();
-      }
-    }
-  });
-  if (widht > 992) {
-    $navbarCollapse.insertAdjacentHTML("afterend", btnlg);
-  } else {
-    $navbarCollapseUl.insertAdjacentHTML("beforebegin", btnlg);
-  }
-};
-
-window.addEventListener("resize", () => responseWindow());
-window.addEventListener("DOMContentLoaded", () => responseWindow());
